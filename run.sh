e26c1e7a5d902a4d8d8d357406719dea25a58bc6
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-04-01 19:30:21 -0400

    change for docker

--- run.sh
+++ run.sh
@@ -7,19 +7,20 @@ source ../config.sh
 source ./config/config.sh
 #add global then local config parameters
 
-podman stop $NAME
-podman rm $NAME
-#stop and remove an existing instance
+docker stop $NAME
+docker rm $NAME
 
-podman run -d \
+docker run -d \
   --name $NAME \
+  -h $HOST \
   --network $NETWORK \
+  -m $MEM_LIMIT \
   --restart $RESTART_MODE \
   -e TZ=$TZ \
-  -e PUID=$PUID \
-  -e PGID=$PGID \
   --dns=$DNS \
   -v $LOCAL_LOG:$REMOTE_LOG \
   -p $BINDING_IP:$H_PORT:$C_PORT \
   -p $BINDING_IP:$H_PORT:$C_PORT/udp \
+  -p $BINDING_IPv6:$H_PORT:$C_PORT \
+  -p $BINDING_IPv6:$H_PORT:$C_PORT/udp \
   $IMAGE
