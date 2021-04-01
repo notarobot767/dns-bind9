d47f116426139a29f8157053e4f1346226bff2d2
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-04-01 19:07:38 -0400

    added comments on build and inserted variable image

--- build.sh
+++ build.sh
@@ -3,6 +3,14 @@
 cd "${0%/*}"
 #change directory relative to script
 
-podman build -f ./config/dockerfile.conf \
-	-t dns-bind9 \
+source "./config/config.conf"
+docker build \
+	--file "./config/dockerfile.conf" \
+	--tag $IMAGE \
+	--pull \
 	.
+
+#--file: specify dockerfile since in subdirectory
+#--tag: name of the image
+#--pull always pull latest base image
+#https://docs.docker.com/engine/reference/commandline/build/
