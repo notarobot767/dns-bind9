a881552002a9ede3bd850409958c64ff3e87b0ac
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-04-02 16:49:34 -0400

    support for ns2

--- config/config.sh
+++ config/config.sh
@@ -3,15 +3,8 @@
 IMAGE="bind9"
 #image to use
 
-NAME="ns1"
-HOST=$NAME
-#name of container
-
 DNS="127.0.0.1"
 #use self for dns
-
-LOCAL_LOG="/disk2/logs/bind9"
-REMOTE_LOG="/app/log"
 
 H_PORT="53"
 C_PORT=$H_PORT
@@ -20,3 +13,18 @@ C_PORT=$H_PORT
 #dont forget tcp & udp
 
 MEM_LIMIT="4g"
+
+#NS1
+#################
+NAME="ns1"
+HOST=$NAME
+
+LOCAL_LOG="/disk2/logs/bind9"
+REMOTE_LOG="/app/log"
+
+#NS2
+#################
+NS2_NAME="ns2"
+NS2_HOST=$NS2_NAME
+NS2_LOCAL_LOG="/disk2/logs/bind9"
+NS2_REMOTE_LOG="/app/log"
